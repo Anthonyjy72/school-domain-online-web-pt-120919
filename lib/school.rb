a72e278a6e69b689ddcb0934a38c286793c8b7fa
Anthony Ybarra <anthony_jy72@yahoo.com>
2020-01-18 18:43:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,8 @@
 class School
   attr_accessor :name, :roster
   
-def sort 
-  nu_hash = {}
-  roster.each do |x, y| 
-    nu_hash[x] = y.sort 
+    def initialize(name)
+    @name = name
+    @roster = {}
   end 
-    nu_hash
-  end 
-end 
+  
